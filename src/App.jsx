0fda090035bff952d1fe9082142b7eb1312bc079
Joy Brar <joybrar2001@gmail.com>
2024-06-19 11:21:55 +0530

    Best Take done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from 'framer-motion';
 import LoadingScreen from "./components/LoadingScreen";
+import { IoClose } from "react-icons/io5";
 
 import bg1 from './assets/BestTake-1.png';
 import bg2 from './assets/BestTake-2.png';
@@ -11,37 +12,28 @@ import Man1 from './assets/Man-1.png';
 import Man2 from './assets/Man-2.png';
 import Man3 from './assets/Man-3.png';
 import Woman from './assets/Woman.png';
+import ToolTip from './assets/tooltip.png';
 
 const assets = [
   bg1, bg2, bg3,
   Child, Man1, Man2, Man3, Woman,
+  ToolTip,
 ];
 
 function App() {
   const [currentBg, setCurrentBg] = useState(bg1);
+  const [currentButton, setCurrentButton] = useState(Man1);
+  const [disclaimerVisible, setDisclaimerVisible] = useState(false);
 
   const [loadedAssets, setLoadedAssets] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [progress, setProgress] = useState(0);
 
   const [clicked, setClicked] = useState(false);
-  const [reveal, setReveal] = useState(false);
 
   useEffect(() => {
     loadAssets(assets);
   }, []);
-
-  useEffect(() => {
-    if (clicked) {
-      setTimeout(() => {
-        setReveal(true);
-      }, 2000);
-      setTimeout(() => {
-        setReveal(false);
-        setClicked(false);
-      }, 14000);
-    }
-  }, [clicked]);
 
   const loadAssets = async (assets) => {
     const loaded = [];
@@ -61,6 +53,10 @@ function App() {
     console.log("Assets Loaded");
   };
 
+  const handleClick = (bgImage) => {
+    setCurrentBg(bgImage);
+  };
+
   return (
     <div className="w-screen h-screen px-5 box-border flex flex-col justify-center items-center gap-7 py-5">
       <div className="unity-canvas box-border relative max-w-full flex flex-col justify-between overflow-hidden mx-auto aspect-ratio-16/9 h-[80vh] w-[45vh] px-4 pt-12 pb-2">
@@ -71,15 +67,6 @@ function App() {
             <div className="w-full h-full absolute inset-0">
               <img src={currentBg} className="object-cover w-full h-full object-left" />
             </div>
-            {/* <div className="absolute inset-0 overflow-hidden h-full w-full">
-              <motion.div
-                animate={reveal ? { width: 0, height: "100%" } : { width: "100%", height: "100%" }}
-                transition={{ duration: 1 }}
-                className="absolute inset-0 left-0 h-full w-full border-r-2 border-white"
-              >
-                <img src={Dark} className="absolute left-0 w-full h-full object-left object-cover" />
-              </motion.div>
-            </div> */}
 
             <div className="text-white text-center font-googleSans z-50 flex flex-col gap-2 md:gap-4">
               <h1 className="text-3xl md:text-4xl font-medium tracking-wide">Best Take</h1>
@@ -90,75 +77,88 @@ function App() {
               </p>
             </div>
 
-            <div className="absolute w-full left-0 right-0 bottom-0 h-64 bg-[#141414] rounded-t-2xl">
+            <div className="absolute w-full left-0 right-0 bottom-0 pt-6 bg-[#141414] rounded-t-2xl flex flex-col justify-center items-center">
+              <div className="flex">
+                <div className="relative bg-[#3C4043]">
+                  <div className="w-full h-full p-2 bg-[#141414] rounded-br-xl">
+                    <img src={Woman} alt="woman" className="h-12 w-12" />
+                  </div>
+                </div>
+                <div className="p-2 bg-[#3C4043] rounded-t-full relative">
+                  {!clicked && (
+                    <img src={ToolTip} className="absolute -translate-x-[50%] left-[50%] -top-12 scale-[300%]" />
+                  )}
+                  <img src={currentButton} alt="current button" className="h-12 w-12" />
+                </div>
+                <div className="relative bg-[#3C4043]">
+                  <div className="w-full h-full p-2 bg-[#141414] rounded-bl-xl">
+                    <img src={Child} alt="child" className="h-12 w-12" />
+                  </div>
+                </div>
+              </div>
 
+              <div className="flex bg-[#3C4043] rounded-full p-2 gap-1.5">
+                <div className="cursor-pointer" onClick={() => handleClick(bg1)}>
+                  <img src={Man1} alt="man1" className={`h-12 w-12 transition border-2 rounded-full ${currentBg === bg1 ? "border-white" : "border-transparent"}`} />
+                </div>
+
+                <div className="relative cursor-pointer" onClick={() => {
+                  handleClick(bg2)
+                  setClicked(true);
+                }}>
+                  {!clicked && (
+                    <motion.div
+                      animate={{
+                        scale: [1, 0.7, 1]
+                      }}
+                      transition={{
+                        duration: 1.5,
+                        ease: "easeInOut",
+                        times: [0, 0.75, 1.5],
+                        repeat: Infinity,
+                      }}
+                      className='absolute inset-0 rounded-full transition duration-150 bg-[#F3B702] bg-opacity-65 border-2 border-[#F0C225]'
+                    />
+                  )}
+                  <img src={Man2} alt="man2" className={`h-12 w-12 transition border-2 rounded-full ${currentBg === bg2 ? "border-white" : "border-transparent"}`} />
+                </div>
+
+                <div className="cursor-pointer" onClick={() => handleClick(bg3)}>
+                  <img src={Man3} alt="man3" className={`h-12 w-12 transition border-2 rounded-full ${currentBg === bg3 ? "border-white" : "border-transparent"}`} />
+                </div>
+              </div>
+
+              <div className="flex justify-between w-full px-3 py-2 mt-4">
+                <h6 className="text-left text-white text-sm w-full">Screen images simulated.</h6>
+                <h6 className="text-right text-white text-sm w-full underline cursor-pointer" onClick={() => setDisclaimerVisible(true)}>Disclaimer</h6>
+              </div>
             </div>
-            {/*
-            <div className="relative text-white z-50 font-googleSans flex flex-col justify-center items-center">
-              <div>
-                {!clicked && (
-                  <img
-                    src={Tooltip}
-                    className="absolute -top-16 h-14 md:h-[4.5rem] md:-top-[5rem] left-[50%] -translate-x-[50%]"
-                    alt="Tooltip"
-                  />
-                )}
-                {!reveal && (
-                  <div
-                    className="relative h-16 w-16 cursor-pointer"
-                    onClick={() => setClicked(true)}
-                  >
-                    {clicked && (
-                      <motion.svg
-                        key="motion-circle"
-                        className="absolute inset-0 w-full h-full"
-                        viewBox="0 0 100 100"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <motion.circle
-                          cx="50"
-                          cy="50"
-                          r="48"
-                          initial={{ pathLength: 0 }}
-                          animate={{ pathLength: 1 }}
-                          exit={{ pathLength: 0 }}
-                          transition={{ duration: 2 }}
-                          className="stroke-[5px] stroke-current"
-                          style={{ transformBox: "fill-box" }}
-                        />
-                      </motion.svg>
-                    )}
 
-                    <div className="absolute inset-0 flex justify-center items-center rounded-full p-5">
-                      <div className={`absolute inset-0 ${reveal && "hidden"}`}>
-                        {clicked ? (
-                          <div className="absolute inset-2 rounded-full transition duration-150 bg-[#026AED]/80 scale-100"></div>
-                        ) : (
-                          <div className="border-2 border-white w-full h-full rounded-full">
-                            <motion.div
-                              animate={{
-                                scale: [1, 0.5, 1],
-                              }}
-                              transition={{
-                                duration: 1.5,
-                                ease: "easeInOut",
-                                times: [0, 0.75, 1.5],
-                                repeat: Infinity,
-                              }}
-                              className="absolute inset-0 rounded-full transition duration-150 bg-[#F3B702] bg-opacity-65 border-2 border-[#F0C225]"
-                            ></motion.div>
-                          </div>
-                        )}
-                      </div>
-                      <div className="">
-                        <img src={Moon} alt="moon" />
-                      </div>
+            <AnimatePresence>
+              {disclaimerVisible && (
+                <motion.div
+                  initial={{ y: "100%" }}
+                  animate={{ y: 0 }}
+                  exit={{ y: "100%" }}
+                  transition={{
+                    type: "easeInOut",
+                    duration: 0.4,
+                  }}
+                  className="absolute left-0 right-0 bottom-0 bg-white p-6 rounded-t-2xl"
+                >
+                  <div className="flex justify-between items-center">
+                    <h1 className="text-[#202124] font-medium">Disclaimers</h1>
+                    <div className="bg-[#3C4043] p-2 rounded-full cursor-pointer" onClick={() => setDisclaimerVisible(false)}>
+                      <IoClose size={18} color="white" />
                     </div>
                   </div>
-                )}
-              </div>
-              <h6 className="text-left max-sm:text-sm w-full mt-[30%]">Screen Image Simulated</h6>
-            </div> */}
+                  <p className="text-[#80868B]">
+                    <sup>1</sup>
+                    Requires Google Photos app.
+                  </p>
+                </motion.div>
+              )}
+            </AnimatePresence>
           </>
         )}
       </div>
